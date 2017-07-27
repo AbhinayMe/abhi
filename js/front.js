48b831f1cd91029bcd9c2dfe88bf36487e9bebdc
Abhinay M <abhi@rentmarket.com.my>
2017-07-27 15:19:28 +0800

    Few minor changes

--- js/front.js
+++ js/front.js
@@ -346,11 +346,41 @@ function fullScreenContainer() {
 
 function map() {
 
-    var styles = [{"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]}, {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}];
+    var styles = [
+    			{
+		    		"featureType": "landscape", 
+    				"stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]}, 
+    			{
+    				"featureType": "poi", 
+    				"stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, 
+    			{
+    				"featureType": "road.highway", 
+    				"stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, 
+    			{
+    				"featureType": "road.arterial", 
+    				"stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, 
+    			{
+    				"featureType": "road.local", 
+    				"stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, 
+    			{	
+    				"featureType": "transit", 
+    				"stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, 
+    			{
+    				"featureType": "administrative.province", 
+    				"stylers": [{"visibility": "off"}]}, 
+    			{
+    				"featureType": "water", 
+    				"elementType": "labels", 
+    				"stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, 
+    			{
+    				"featureType": "water", 
+    				"elementType": "geometry", 
+    				"stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}
+    				];
     map = new GMaps({
 	el: '#map',
-	lat: -12.043333,
-	lng: -77.028333,
+	lat: 11.5527408,
+	lng: 101.6169491,
 	zoomControl: true,
 	zoomControlOpt: {
 	    style: 'SMALL',
@@ -368,10 +398,20 @@ function map() {
     var image = 'img/marker.png';
 
     map.addMarker({
-	lat: -12.043333,
-	lng: -77.028333,
-	icon: image/* ,
-	 title: '',
+	lat: 17.4121531,
+	lng: 78.1278513,
+	icon: image,
+	 title: 'Hyderabad, India'/* ,
+	 infoWindow: {
+	 content: '<p>HTML Content</p>'
+	 }*/
+    });
+    
+    map.addMarker({
+	lat: 3.1385036,
+	lng: 101.6169491,
+	icon: image,
+	 title: 'Kuala Lumpur, Malaysia'/* ,
 	 infoWindow: {
 	 content: '<p>HTML Content</p>'
 	 }*/
